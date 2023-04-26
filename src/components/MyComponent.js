70511ad63f2afd12aa761f82d1365251d85e96b2
Khoa Trinh <trinhleanhkhoa1999@gmail.com>
2023-04-26 19:21:37 +0700

    video20

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -6,10 +6,26 @@ export class MyComponent extends React.Component {
     age: 24,
     address: "Tp.Hcm",
   };
+  handleClick = (event) => {
+    console.log("handleClick");
+    console.log("My name is ", this.state.name);
+
+    this.setState({
+      name: "be bi",
+      age: Math.floor(Math.random() * 10) + 1,
+    });
+  };
   render() {
     return (
       <div>
         My name is {this.state.name} and my age {this.state.age}
+        <button
+          onClick={(event) => {
+            this.handleClick(event);
+          }}
+        >
+          Click me!
+        </button>
       </div>
     );
   }
