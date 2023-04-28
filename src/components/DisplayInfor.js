47559e78860af307c2fde612b0a378764b37a776
Khoa Trinh <trinhleanhkhoa1999@gmail.com>
2023-04-28 13:51:32 +0700

    video 34 function component

--- src/components/DisplayInfor.js
+++ src/components/DisplayInfor.js
@@ -1,75 +1,72 @@
 import React, { Component } from "react";
 import "./DisplayInfor.scss";
 
-export default class DisplayInfor extends Component {
-  constructor(props) {
-    console.log(">>> call constructor: 1");
-    super(props);
-    this.state = {
-      isShowHide: true,
-    };
-  }
-
-  //set true or false
-  handleShowHide = () => {
-    this.setState({
-      isShowHide: !this.state.isShowHide,
-    });
-  };
-  componentDidMount() {
-    console.log(">>> call me componentDidMount");
-    setTimeout(() => {
-      document.title = "Test changed title";
-    }, 3000);
-  }
-  componentDidUpdate() {
-    console.log(">>> call me componentDidUpdate");
-  }
-
-  render() {
-    console.log(">>> call render");
-    //Destructoring Array/Object
-    const { listUsers } = this.props;
-    //listUsers = this.props.listUsers
-    return (
-      <div className="display-infor-container">
-        <div>
-          <span
-            onClick={() => {
-              this.handleShowHide();
-            }}
-          >
-            {this.state.isShowHide === true
-              ? "Hide List User:"
-              : "Show List User:"}
-          </span>
-        </div>
-        {this.state.isShowHide && (
-          <>
-            {listUsers.map((item, idx) => {
-              return (
-                <div
-                  key={item.id}
-                  className={+item.age <= 18 ? "green" : "red"}
-                >
-                  <div> My name is {item.name}</div>
-                  <div> My age {item.age}</div>
-                  <div>
-                    <button
-                      onClick={() => {
-                        this.props.handleDeleteUser(item.id);
-                      }}
-                    >
-                      Delete!
-                    </button>
-                  </div>
-                  <hr />
+// class DisplayInfor extends Component {
+//   render() {
+//     console.log(">>> call render");
+//     //Destructoring Array/Object
+//     const { listUsers } = this.props;
+//     //listUsers = this.props.listUsers
+//     return (
+//       <div className="display-infor-container">
+//         {true && (
+//           <>
+//             {listUsers.map((item, idx) => {
+//               return (
+//                 <div
+//                   key={item.id}
+//                   className={+item.age <= 18 ? "green" : "red"}
+//                 >
+//                   <div> My name is {item.name}</div>
+//                   <div> My age {item.age}</div>
+//                   <div>
+//                     <button
+//                       onClick={() => {
+//                         this.props.handleDeleteUser(item.id);
+//                       }}
+//                     >
+//                       Delete!
+//                     </button>
+//                   </div>
+//                   <hr />
+//                 </div>
+//               );
+//             })}
+//           </>
+//         )}
+//       </div>
+//     );
+//   }
+// }
+const DisplayInfor = (props) => {
+  //Destructoring Array/Object
+  const { listUsers } = props; //object
+  return (
+    <div className="display-infor-container">
+      {true && (
+        <>
+          {listUsers.map((item, idx) => {
+            return (
+              <div key={item.id} className={+item.age <= 18 ? "green" : "red"}>
+                <div> My name is {item.name}</div>
+                <div> My age {item.age}</div>
+                <div>
+                  <button
+                    onClick={() => {
+                      props.handleDeleteUser(item.id);
+                    }}
+                  >
+                    Delete!
+                  </button>
                 </div>
-              );
-            })}
-          </>
-        )}
-      </div>
-    );
-  }
-}
+                <hr />
+              </div>
+            );
+          })}
+        </>
+      )}
+    </div>
+  );
+};
+
+export default DisplayInfor;
