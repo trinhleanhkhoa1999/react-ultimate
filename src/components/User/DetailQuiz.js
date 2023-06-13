ac8c78f1065af6584b7a6c4b42378606016a17bd
Khoa Trinh <trinhleanhkhoa1999@gmail.com>
2023-06-13 15:32:11 +0700

    102. #84. Design Quiz Layout - Tạo Base Giao Diện Bài Thi

--- src/components/User/DetailQuiz.js
+++ src/components/User/DetailQuiz.js
@@ -1,19 +1,69 @@
 import { useEffect } from "react";
-import { useParams } from "react-router-dom";
+import { useParams, useLocation } from "react-router-dom";
 import { getDataQuiz } from "../../services/apiService";
+import _ from "lodash";
+import "./DetailQuiz.scss";
 const DetailQuiz = (props) => {
   const params = useParams();
   const quizId = params.id;
-
+  const location = useLocation();
   useEffect(() => {
     fetchQuestions();
   }, [quizId]);
 
   const fetchQuestions = async () => {
     let res = await getDataQuiz(quizId);
-    console.log("check res data quiz", res);
+
+    if (res && res.EC === 0) {
+      let raw = res.DT;
+      let data = _.chain(raw)
+        // Group the elements of Array based on `color` property
+        .groupBy("id")
+        // `key` is group's name (color), `value` is the array of objects
+        .map((value, key) => {
+          let answers = [];
+          let questionDescription,
+            image = null;
+          value.forEach((item, index) => {
+            if (index === 0) {
+              questionDescription = item.description;
+              image = item.image;
+            }
+            answers.push(item.answers);
+          });
+
+          return { questionId: key, answers, questionDescription, image };
+        })
+        .value();
+      console.log("data", data);
+    }
   };
 
-  return <div className="detail-quiz-container">DetailQuiz</div>;
+  return (
+    <div className="detail-quiz-container">
+      <div className="left-content">
+        <div className="title">
+          Quiz {quizId}: {location?.state?.quizTitle}
+        </div>
+        <hr />
+        <div className="q-body">
+          <img />
+        </div>
+        <div className="q-content">
+          <div className="q-question">Question 1: How are you doing?</div>
+          <div className="answer">
+            <div className="a-child">A: adada</div>
+            <div className="a-child">B: adada</div>
+            <div className="a-child">C: adada</div>
+          </div>
+        </div>
+        <div className="footer">
+          <button className="btn btn-primary">prev</button>
+          <button className="btn btn-secondary">next</button>
+        </div>
+      </div>
+      <div className="right-content">count down</div>
+    </div>
+  );
 };
 export default DetailQuiz;
